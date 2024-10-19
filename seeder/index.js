a7fbf738003012f377c0da203293e065f78c71e9
Luji Ramdhan <lrdn03@gmail.com>
2024-10-19 14:08:03 +0700

    feat: bulk-insert

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const fs = require('fs');
+const { Buffer } = require('buffer');
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
@@ -18,7 +20,32 @@ async function main() {
   });
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: Number,
+      required: true
+    },
+    gendre: {
+      type: [String],
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    director: {
+      type: String,
+      required: true
+    },
+    cast: {
+      type: [String],
+      required: true
+    },
+  }, { strict: false });
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
@@ -26,6 +53,9 @@ async function main() {
       await checkConnection();
       break;
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    case "bulk-insert" :
+      await insertAllMovies(Model);
+      break;
     default:
       throw Error("command not found");
   }
@@ -43,6 +73,16 @@ async function checkConnection() {
     console.error("MongoDB connection failed:", err);
   }
   console.log("check db connection ended...");
+}
+
+
+async function insertAllMovies(model) {
+  let seed = fs.readFileSync('seed.json');
+  seed = Buffer.from(seed);
+  seed = seed.toString();
+  seed = JSON.parse(seed);
+  await model.insertMany(seed);
+  console.log('successfully insert all documents');
 }
 
 main();
